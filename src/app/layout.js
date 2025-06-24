01f9945ea7a22866af1e7d3352b892bd2680603d
Naresh Choudhary <nareshgorachiya@gmail.com>
2025-06-24 21:33:34 +0530

    Update layout.js testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Loader />
+        // <Loader />
         {children}
       </body>
     </html>
